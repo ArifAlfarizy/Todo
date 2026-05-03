2eec72986942dbabb5df34ba45086ffbc51db947
Alfarizy <arifalfrzy@gmail.com>
2026-05-03 20:54:53 +0700

    Initial commit

---
+++ server/src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("tes");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`)
+})
